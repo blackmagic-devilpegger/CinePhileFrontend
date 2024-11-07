12aa0b224aac3a326d64c48d96fc3d7f239bc2ad
blackmagic-devilpegger <166638520+blackmagic-devilpegger@users.noreply.github.com>
2024-11-07 19:05:31 +0100

    heute

---
+++ src/components/FilmCompositionApi.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+defineProps<{ title: string }>()
+let inputData = ref('das ist ein Input')
+
+function saveFilm(value: string): void {
+  films.value.push(inputData.value)
+}
+
+// hook
+onMounted(() => {
+  films.value.push('Film 1')
+})
+
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+  <button @click="saveFilm()">Change title</button>
+  <input v-model="inputData"/>
+
+  <p v-if="films.length < 1" class="warning">No films</p>
+
+  <ul>
+    <li v-for="film in films" :key="film.id">{{ film.title }}</li>
+  </ul>
+
+</template>
+
+<style scoped>
+.warning{
+  color: red;
+
+</style>
---
+++ src/components/FilmOptionApiList.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'FilmOptionApiList',
+  props: ['Title'],
+  data(inputData:string, films: string[]) {
+    return {
+      inputData: '',
+      films: [],
+    }
+  },
+  methods: {
+    saveFilm(){
+      this.films.push(this.inputData)
+})
+</script>
+
+<template>
+  <div>
+    <h1>FilmOptionApiList</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
