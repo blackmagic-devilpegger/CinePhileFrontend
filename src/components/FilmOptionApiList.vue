66cc1ae67ed80a43b82d35dc0640b2e00672862b
philine <philine.rolke@outlook.de>
2024-11-28 20:59:56 +0100

    ganz viel zu m3

--- src/components/FilmOptionApiList.vue
+++ src/components/FilmOptionApiList.vue
@@ -1,10 +1,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import axios from 'axios'
+import type { Film } from '@/model/Film'
 
 export default defineComponent({
   name: 'FilmOptionApiList',
   props: ['Title'],
-  data(): { inputData: string, films: string[] } {
+  data(): { inputData: string, films: Film[] } {
     return {
       inputData: '',
       films: [],
@@ -12,9 +14,15 @@ export default defineComponent({
   },
   methods: {
     saveFilm(){
-      this.films.push(this.inputData)
+      this.films.push({title: this.inputData, year: 2021})
     }
-  }
+  },
+  mounted() {
+    axios
+      .get<Film[] >('http://localhost:8080/films')
+      .then((response) => (this.films =response.data))
+      .catch((error) => console.log(error))
+  },
 })
 </script>
 
---
+++ src/model/Film.ts
@@ -0,0 +1,4 @@
+export interface Film {
+  year: number,
+  title: string
+}
